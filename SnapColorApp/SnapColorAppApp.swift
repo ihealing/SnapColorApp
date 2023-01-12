7b550727d0c71b735cc367a257db267a73fef891
ihealing <shifaabdulaziz@hotmail.com>
2023-01-12 11:58:49 +0300

    Initial Commit

---
+++ SnapColorApp/SnapColorAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnapColorAppApp.swift
+//  SnapColorApp
+//
+//  Created by Shifa Alfaisal on 19/06/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct SnapColorAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
