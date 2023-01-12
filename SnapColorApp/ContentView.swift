9c0cdf68d4e2c5952af8a9fe1db318ec3e0121d4
ihealing <shifaabdulaziz@hotmail.com>
2023-01-12 12:03:41 +0300

    Shifa

--- SnapColorApp/ContentView.swift
+++ SnapColorApp/ContentView.swift
@@ -8,16 +8,62 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
-}
+    static var shouldDifferentiateWithoutColor: Bool  = false
+    let names = ["Social media", "Desert", "Grass", "Rainbow", "Flower", "Love", "Water" ]
+    @State private var searchText = ""
+    @State private var isPresented = false
+    @State private var isPresented1 = false
+        @State private var isPresented2 = false
+
+        
+
+            @State var text = ""
+            var body: some View {
+                NavigationView {
+                    
+                    VStack {
+                        SearchBar(text: $text)
+                            .padding()
+                        
+                        List {
+                            ForEach(names.filter({"\($0)".contains(text) }), id: \.self) { name in
+                                Button {
+                                    Text(name)
+                                    isPresented1 .toggle()
+                                } label: {
+                                    Text(name)
+                                }
+                                Button {
+                                    Text(name)
+                                    isPresented2 .toggle()
+                                } label: {
+                                    Text(name)
+                                }
+                            }
+                            
+                        }
+                        .listStyle(.plain)
+                        .frame(width: 10,height: 10)
+                        .fullScreenCover(isPresented: $isPresented1)
+                        {colorpalettebasedonword()}
+                            .fullScreenCover(isPresented: $isPresented2)
+                            {Lovesheet()}
+    //                        .frame(width: 100, height: 50)
+                        VStack{
+                            Colorsline1()
+                                                  Spacer()
+                                .padding()
+
+                            Colorsline2()
+    //                        Spacer()
+                            .padding(40)
+                        }
+
+
+                    }
+                }
+                    }
+                }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
--- SnapColorApp/SnapColorAppApp.swift
+++ SnapColorApp/SnapColorAppApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct SnapColorAppApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            Splash()
         }
     }
 }
